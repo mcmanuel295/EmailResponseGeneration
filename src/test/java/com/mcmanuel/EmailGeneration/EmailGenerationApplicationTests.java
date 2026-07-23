1e5d3a507ec8d95869dbb8f713770ea8fc6ae63d
unknown <mcmanuel755@gmail.com>
2026-07-23 06:25:30 +0100

    test folder added

---
+++ src/test/java/com/mcmanuel/EmailGeneration/EmailGenerationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mcmanuel.EmailGeneration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmailGenerationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
