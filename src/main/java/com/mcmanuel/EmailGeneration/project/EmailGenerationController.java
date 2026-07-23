80ad943f55d5542f3d4ead1cc149aa9494a10e94
unknown <mcmanuel755@gmail.com>
2026-07-23 06:52:58 +0100

    added ai configuration

--- src/main/java/com/mcmanuel/EmailGeneration/project/EmailGenerationController.java
+++ src/main/java/com/mcmanuel/EmailGeneration/project/EmailGenerationController.java
@@ -3,16 +3,18 @@ package com.mcmanuel.EmailGeneration.project;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api/email/generate")
+@RequestMapping("/api/email")
 public class EmailGenerationController {
     private final EmailGenerationService service;
 
+    @PostMapping("/generate")
     public ResponseEntity<String> generateEmailResponse(@RequestBody EmailRequest emailRequest){
         return new ResponseEntity<>(service.generate(emailRequest), HttpStatus.OK);
     }
