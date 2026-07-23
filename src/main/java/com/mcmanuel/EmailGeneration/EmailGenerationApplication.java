072040438fab119b38cec93bf847b393c990c479
unknown <mcmanuel755@gmail.com>
2026-07-23 06:23:51 +0100

    added files

---
+++ src/main/java/com/mcmanuel/EmailGeneration/EmailGenerationApplication.java
@@ -0,0 +1,13 @@
+package com.mcmanuel.EmailGeneration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmailGenerationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmailGenerationApplication.class, args);
+	}
+
+}
