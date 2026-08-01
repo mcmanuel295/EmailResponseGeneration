ca13fd6de1917a0888b85e5d71cf9d4389906e7c
unknown <mcmanuel755@gmail.com>
2026-08-01 15:54:45 +0100

    added cors configuration

--- src/main/java/com/mcmanuel/EmailGeneration/project/SecurityConfiguration.java
+++ src/main/java/com/mcmanuel/EmailGeneration/project/SecurityConfiguration.java
@@ -6,23 +6,36 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.util.Arrays;
 import java.util.Collections;
 
 @Service
 @Configuration
-public class SecurityConfiguration {
+public class SecurityConfiguration implements WebMvcConfigurer {
 
-    @Bean
-    public CorsFilter corsConfig(){
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(Arrays.asList("https://google.com","http://127.0.0.1:5500/src/main/resources/static/**"));
-        config.setAllowedHeaders(Collections.singletonList("*"));
-        config.setAllowedMethods(Arrays.asList("POST","GET","DELETE"));
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**") // Apply to all endpoints
+                .allowedOrigins("https://google.com", "http://127.0.0.1:5500", "http://localhost:5500")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .exposedHeaders("Access-Control-Allow-Origin")
+                .allowCredentials(true);
 
-        source.registerCorsConfiguration("/**",config);
-        return new CorsFilter(source);
     }
+
+//    @Bean
+//    public CorsFilter corsConfig(){
+//        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+//        CorsConfiguration config = new CorsConfiguration();
+//        config.setAllowedOrigins(Arrays.asList("https://google.com","http://127.0.0.1:5500/src/main/resources/static/**"));
+//        config.setAllowedHeaders(Collections.singletonList("*"));
+//        config.setAllowedMethods(Arrays.asList("POST","GET","DELETE"));
+//
+//        source.registerCorsConfiguration("/**",config);
+//        return new CorsFilter(source);
+//    }
 }
